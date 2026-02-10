d3a7a53750b07d6dd531bffaa04f3ef7e1519452
katarinasokolova <katarina_sokolova06@mail.ru>
2026-02-10 19:28:16 +0300

    Delete project2/task_2_2/task_2-2_inventory_control.py

--- project2/task_2_2/task_2-2_inventory_control.py
+++
@@ -1,4 +0,0 @@
-reagent_name = input("Введите название нового реактива: ")
-reagent_quantity = input("Введите количество реагента: ")
-print(f"Реактив {reagent_name} поступил на склад в количестве {reagent_quantity}.")
-
