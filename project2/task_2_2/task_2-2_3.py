23551b40d249040ee27ddff608d6be261fe23721
katarinasokolova <katarina_sokolova06@mail.ru>
2026-02-10 19:28:08 +0300

    Delete project2/task_2_2/task_2-2_3.py

--- project2/task_2_2/task_2-2_3.py
+++
@@ -1,6 +0,0 @@
-name_device = "Осциллограф"
-inventory_number = "INV-2024-087"
-operable = "Исправен"
-quantity = 3
-print("Название прибора\tИнвентарный номер\tСостояние\tКоличество")
-print(name_device, inventory_number, operable, quantity, sep = "\t")
