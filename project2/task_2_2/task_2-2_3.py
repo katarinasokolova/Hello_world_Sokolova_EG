ec0f3452900026ac91b8ff20ecb0fcb7f70e1cc2
katarinasokolova <katarina_sokolova06@mail.ru>
2026-02-10 15:04:19 +0300

    добавила задание 2.2

---
+++ project2/task_2_2/task_2-2_3.py
@@ -0,0 +1,6 @@
+name_device = "Осциллограф"
+inventory_number = "INV-2024-087"
+operable = "Исправен"
+quantity = 3
+print("Название прибора\tИнвентарный номер\tСостояние\tКоличество")
+print(name_device, inventory_number, operable, quantity, sep = "\t")
---
+++ project2/task_2_2/task_2-2_inventory_control.py
@@ -0,0 +1,3 @@
+reagent_name = input("Введите название нового реактива: ")
+reagent_quantity = int(input("Введите количество реагента: "))
+print(f"Реактив {reagent_name} поступил на склад в количестве {reagent_quantity}.")
