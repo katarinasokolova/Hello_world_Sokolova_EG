4c08c0256dc4f96f970c8bc50471690f3c3cc4e9
katarinasokolova <katarina_sokolova06@mail.ru>
2026-02-10 19:07:33 +0300

    Update task_2-2_inventory_control.py

--- project2/task_2_2/task_2-2_inventory_control.py
+++ project2/task_2_2/task_2-2_inventory_control.py
@@ -1,3 +1,4 @@
 reagent_name = input("Введите название нового реактива: ")
-reagent_quantity = int(input("Введите количество реагента: "))
+reagent_quantity = input("Введите количество реагента: ")
 print(f"Реактив {reagent_name} поступил на склад в количестве {reagent_quantity}.")
+
