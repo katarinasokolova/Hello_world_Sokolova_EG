4cb1f502e248e71d732a1651b465f5723891c4e4
katarinasokolova <katarina_sokolova06@mail.ru>
2026-03-06 07:30:22 +0300

    добавила задание 3.11

---
+++ projects_3/task_3_11/check_root.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+check_root() {
+	if [ $EUID -ne 0 ]; then
+		echo "Ошибка: скрипт должен быть запущен от имени суперпользователя (root)."
+		exit 1
+	fi
+}
+check_root
+echo "Скрипт запущен от имени root."
